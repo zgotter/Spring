ec014aa0a792fcb82dbb3bc71c1e1b515326ee5e
shkim4738 <shkim4738@gmail.com>
2020-01-24 02:55:19 +0900

    스프링 학습 전 필요 지식 전체 과정 복습

---
+++ necessary_knowledge/webTest/src/main/java/kr/co/test/cli/Main.java
@@ -0,0 +1,43 @@
+package kr.co.test.cli;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+class Main {
+
+    private static Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) throws ClassNotFoundException {
+
+    	logger.info("Hello World!");
+
+        Class.forName("org.h2.Driver");
+        var url = "jdbc:h2:mem:test;MODE=MySQL";
+
+        try ( var connection = DriverManager.getConnection(url, "sa", "");
+              var statement = connection.createStatement() ) {
+
+            connection.setAutoCommit(false);
+            statement.execute("create table member(id int auto_increment, username varchar(255) not null, password varchar(255) not null, primary key(id))");
+            try {
+                statement.executeUpdate("insert into member (username, password) values ('shkim', '1234')");
+                connection.commit();
+            } catch (SQLException e) {
+                e.printStackTrace();
+                connection.rollback();
+            }
+            var resultSet = statement.executeQuery("select id, username, password from member");
+            while (resultSet.next()) {
+                var member = new Member(resultSet);
+                logger.info(member.toString());
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ necessary_knowledge/webTest/src/main/webapp/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: shkim
+  Date: 2020-01-24
+  Time: 오전 2:07
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Web Test</title>
+</head>
+<body>
+<h2>Hello Web!</h2>
+</body>
+</html>
