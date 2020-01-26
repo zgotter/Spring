f918c645433a53a511a8c8221b53e92339f8bccc
shkim4738 <shkim4738@gmail.com>
2020-01-26 20:07:19 +0900

    spring-context dependency 추가 및 코드로 DAO 구현

---
+++ SpringFramework-Core/spring-web/src/main/java/kr/co/test/cli/Dao.java
@@ -0,0 +1,42 @@
+package kr.co.test.cli;
+
+import lombok.extern.slf4j.Slf4j;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+@Slf4j
+public class Dao { // DAO: Data Access Object
+    public void run() {
+
+        try {
+            Class.forName("org.h2.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        var url = "jdbc:h2:mem:test;MODE=MySQL";
+
+        try ( var connection = DriverManager.getConnection(url, "sa", "");
+              var statement = connection.createStatement() ) {
+
+            connection.setAutoCommit(false);
+            statement.execute("create table member(id int auto_increment, username varchar(255) not null, password varchar(255) not null, primary key(id))");
+            try {
+                statement.executeUpdate("insert into member (username, password) values ('shkim', '1234')");
+                connection.commit();
+            } catch (SQLException e) {
+                e.printStackTrace();
+                connection.rollback();
+            }
+            var resultSet = statement.executeQuery("select id, username, password from member");
+            while (resultSet.next()) {
+                var member = new Member(resultSet);
+                log.info(member.toString());
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
--- SpringFramework-Core/spring-web/src/main/java/kr/co/test/cli/Main.java
+++ SpringFramework-Core/spring-web/src/main/java/kr/co/test/cli/Main.java
@@ -11,34 +11,8 @@ class Main {
     private static Logger logger = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) throws ClassNotFoundException {
-
-    	logger.info("Hello World!");
-
-        Class.forName("org.h2.Driver");
-        var url = "jdbc:h2:mem:test;MODE=MySQL";
-
-        try ( var connection = DriverManager.getConnection(url, "sa", "");
-              var statement = connection.createStatement() ) {
-
-            connection.setAutoCommit(false);
-            statement.execute("create table member(id int auto_increment, username varchar(255) not null, password varchar(255) not null, primary key(id))");
-            try {
-                statement.executeUpdate("insert into member (username, password) values ('shkim', '1234')");
-                statement.executeUpdate("insert into member (username, password) values ('shkim', '5678')");
-                connection.commit();
-            } catch (SQLException e) {
-                e.printStackTrace();
-                connection.rollback();
-            }
-            var resultSet = statement.executeQuery("select id, username, password from member");
-            while (resultSet.next()) {
-                var member = new Member(resultSet);
-                logger.info(member.toString());
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
+        logger.info("Hello World!");
+        Dao dao = new Dao();
+        dao.run();
     }
 }
