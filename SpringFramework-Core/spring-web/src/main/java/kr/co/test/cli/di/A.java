6c846ab6f09b1d15f3bf14faa5b70341a8afebbf
shkim4738 <shkim4738@gmail.com>
2020-01-26 22:31:41 +0900

    객체 간의 의존성 - Main 클래스에서 객체 간의 의존성 주입

--- SpringFramework-Core/spring-web/src/main/java/kr/co/test/cli/di/A.java
+++ SpringFramework-Core/spring-web/src/main/java/kr/co/test/cli/di/A.java
@@ -1,11 +1,11 @@
 package kr.co.test.cli.di;
 
 public class A {
-    // A 클래스는 B 클래스에 의존성이 있다.
+    // DI 원칙은 코드 레벨에서 객체간의 관계를 맺지 않는 것이다.
     private B b;
 
-    public A(boolean condition) {
-        this.b = new B(condition); // B 클래스를 수정하기 위해서 A 클래스의 코드 레벨을 변경해야 한다. (A와 B의 의존성 때문
+    public A(B b) {
+        this.b = b;
     }
 
     public void print() {
--- SpringFramework-Core/spring-web/src/main/java/kr/co/test/cli/di/Main.java
+++ SpringFramework-Core/spring-web/src/main/java/kr/co/test/cli/di/Main.java
@@ -3,9 +3,12 @@ package kr.co.test.cli.di;
 public class Main {
     public static void main(String[] args) {
 
-        boolean condition = true; // Main 에서 true, false를 제어하기 위해서는 A 클래스와 B 클래스의 코드 복잡성이 높아진다.
-        // DI를 통해 객체 간의 높은 의존성을 디커플링하여 유지보수의 효율성을 높일 필요가 있다.
-        A a = new A(condition);
+        boolean condition = true;
+
+        // A 객체의 B의 의존성 주입 (Dependency Injection)
+        // - Main 클래스에서 객체 간의 의존성을 주입한다.
+        B b = new B(condition);
+        A a = new A(b); // A 클래스는 B 클래스가 가지는 condition 값을 몰라도 된다.
         a.print();
     }
 }
