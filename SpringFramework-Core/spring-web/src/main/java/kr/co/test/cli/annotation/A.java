e085f1678fbb309c958b2028f9d00af5aa670611
shkim4738 <shkim4738@gmail.com>
2020-01-27 21:42:26 +0900

    @Autowired - Field

--- SpringFramework-Core/spring-web/src/main/java/kr/co/test/cli/annotation/A.java
+++ SpringFramework-Core/spring-web/src/main/java/kr/co/test/cli/annotation/A.java
@@ -3,12 +3,14 @@ package kr.co.test.cli.annotation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import javax.annotation.PostConstruct;
+
 @Slf4j
 public class A {
-    private B b;
+    @Autowired private B b;
 
-    @Autowired
-    public void setB(B b) {
-        this.b = b;
+    @PostConstruct
+    void init() {
+        log.info("" + b);
     }
 }
