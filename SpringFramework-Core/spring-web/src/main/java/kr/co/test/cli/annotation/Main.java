2cdc846e7c1d59dd29560e0a62ebb641469d9d86
shkim4738 <shkim4738@gmail.com>
2020-01-30 20:59:55 +0900

    excludeFilter 사용 - 필터타입-regex

--- SpringFramework-Core/spring-web/src/main/java/kr/co/test/cli/annotation/Main.java
+++ SpringFramework-Core/spring-web/src/main/java/kr/co/test/cli/annotation/Main.java
@@ -10,13 +10,13 @@ import org.springframework.context.annotation.FilterType;
 
 @Slf4j
 @Configuration
-@ComponentScan(basePackageClasses = Main.class, excludeFilters = @Filter(type= FilterType.ASSIGNABLE_TYPE, classes = B.class))
+@ComponentScan(basePackageClasses = Main.class, excludeFilters = @Filter(type= FilterType.REGEX, pattern = "kr.co.test.cli.annotation.B"))
 public class Main {
     public static void main(String[] args) {
         log.info("Hello World");
         ConfigurableApplicationContext context = new AnnotationConfigApplicationContext(Main.class); // 패키지 경로를 문자열보다 타입 값을 넣어주는 게 좋다.
         B b = context.getBean(B.class); // B 클래스가 excludeFilters로 지정됐기 때문에 b에 대한 bean이 생성되어 있지 않아 에러가 발생한다.
-        log.info("" + b); // B 가 있는 지 확인 
+        log.info("" + b); // B 가 있는 지 확인
 
         context.close();
     }
