515e3d23825cdfc57e82ddfd01066ccfcac33e08
shkim4738 <shkim4738@gmail.com>
2020-02-02 19:24:22 +0900

    @PropertySource annotation을 이용한 properties 파일 사용

--- SpringFramework-Core/spring-web/src/main/java/kr/co/test/cli/AppDefaultConfig.java
+++ SpringFramework-Core/spring-web/src/main/java/kr/co/test/cli/AppDefaultConfig.java
@@ -1,16 +1,21 @@
 package kr.co.test.cli;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
 
-import java.sql.Connection;
 
 @Configuration
 @Profile("default")
+@PropertySource("classpath:application.properties")
 public class AppDefaultConfig {
     @Bean(initMethod = "init", destroyMethod = "destroy")
-    public ConnectionFactory connectionFactory() {
-        return new ConnectionFactory("org.h2.Driver", "jdbc:h2:mem:test", "sa", "");
+    public ConnectionFactory connectionFactory(@Value("${jdbc.driver-class}") String driverClass,
+                                               @Value("${jdbc.url}") String url,
+                                               @Value("${jdbc.username}") String username,
+                                               @Value("${jdbc.password}") String password) {
+        return new ConnectionFactory(driverClass, url, username, password);
     }
 }
--- SpringFramework-Core/spring-web/src/main/java/kr/co/test/cli/AppDevConfig.java
+++ SpringFramework-Core/spring-web/src/main/java/kr/co/test/cli/AppDevConfig.java
@@ -1,14 +1,20 @@
 package kr.co.test.cli;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @Profile("dev")
+@PropertySource("classpath:application-dev.properties")
 public class AppDevConfig {
     @Bean(initMethod = "init", destroyMethod = "destroy")
-    public ConnectionFactory connectionFactory() {
-        return new ConnectionFactory("org.h2.Driver", "jdbc:h2:mem:file:~/test", "sa", "");
+    public ConnectionFactory connectionFactory(@Value("${jdbc.driver-class}") String driverClass,
+                                               @Value("${jdbc.url}") String url,
+                                               @Value("${jdbc.username}") String username,
+                                               @Value("${jdbc.password}") String password) {
+        return new ConnectionFactory(driverClass, url, username, password);
     }
 }
