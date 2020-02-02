80557b019eed057adb5b778e50fe1f7f6bf8f558
shkim4738 <shkim4738@gmail.com>
2020-02-02 19:51:44 +0900

    class.getClassLoader().getResourceAsStream() 이용 파일 일기

--- SpringFramework-Core/spring-web/src/main/java/kr/co/test/cli/res/Main.java
+++ SpringFramework-Core/spring-web/src/main/java/kr/co/test/cli/res/Main.java
@@ -4,11 +4,9 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        String path = "C:\\shkim\\intellij\\Spring\\SpringFramework-Core\\spring-web\\src\\main\\resources\\bean.xml";
-        File file = new File(path);
-        InputStream is = new FileInputStream(file);
+        InputStream is = Main.class.getClassLoader().getResourceAsStream("dao.xml");
         byte[] bytes = is.readAllBytes();
         String daoStr = new String(bytes);
         System.out.println(daoStr);
     }
-}
+} 
