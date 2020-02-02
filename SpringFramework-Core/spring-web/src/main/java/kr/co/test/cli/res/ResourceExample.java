6044914a342aa9d2f50021b3f01eba82a6ac9788
shkim4738 <shkim4738@gmail.com>
2020-02-02 22:04:38 +0900

    @Autowired annotation을 이용한 ResourceLoader 사용

--- SpringFramework-Core/spring-web/src/main/java/kr/co/test/cli/res/ResourceExample.java
+++ SpringFramework-Core/spring-web/src/main/java/kr/co/test/cli/res/ResourceExample.java
@@ -6,13 +6,8 @@ import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
 
 @Component
-public class ResourceExample implements ResourceLoaderAware {
-    private ResourceLoader resourceLoader;
-
-    @Override
-    public void setResourceLoader(ResourceLoader resourceLoader) {
-        this.resourceLoader = resourceLoader;
-    }
+public class ResourceExample {
+    @Autowired private ResourceLoader resourceLoader;
 
     public void print() {
         System.out.println(resourceLoader);
