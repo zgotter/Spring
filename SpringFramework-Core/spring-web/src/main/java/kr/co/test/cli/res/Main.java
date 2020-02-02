157e1f0a2cd7c31c6d77b427c0df1156657b881d
shkim4738 <shkim4738@gmail.com>
2020-02-02 21:52:24 +0900

    ResourceLoader - "classpath:"

--- SpringFramework-Core/spring-web/src/main/java/kr/co/test/cli/res/Main.java
+++ SpringFramework-Core/spring-web/src/main/java/kr/co/test/cli/res/Main.java
@@ -1,16 +1,14 @@
 package kr.co.test.cli.res;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
 
 import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext();
-        Resource resource = context.getResource("dao.xml");
+        Resource resource = context.getResource("classpath:dao.xml");
         String daoStr = new String(resource.getInputStream().readAllBytes());
         System.out.println(daoStr);
         context.close();
