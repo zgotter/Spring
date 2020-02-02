de2531a4dc0bf813eb4116559ec120326f0f8ed7
shkim4738 <shkim4738@gmail.com>
2020-02-02 20:24:44 +0900

    UrlResource 사용

--- SpringFramework-Core/spring-web/src/main/java/kr/co/test/cli/res/Main.java
+++ SpringFramework-Core/spring-web/src/main/java/kr/co/test/cli/res/Main.java
@@ -1,15 +1,13 @@
 package kr.co.test.cli.res;
 
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.UrlResource;
 
 import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        ClassPathResource resource = new ClassPathResource("dao.xml");
-        System.out.println(resource.exists()); // 파일이 있는 지 확인
-        byte[] bytes = resource.getInputStream().readAllBytes();
-        String daoStr = new String(bytes);
-        System.out.println(daoStr);
+        // UrlResource는 절대경로를 명시해줘야 한다.
+        UrlResource resource = new UrlResource("file:C:\\shkim\\intellij\\Spring\\SpringFramework-Core\\spring-web\\src\\main\\resources\\dao.xml");
+        System.out.println(resource.exists());
     }
 }
