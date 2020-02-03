fc245403fe2dd4c477d7dfc232bf686bd422cbb2
shkim4738 <shkim4738@gmail.com>
2020-02-03 22:08:00 +0900

    proxy 실습 - proxy 적용

--- SpringFramework-Core/spring-web/src/main/java/kr/co/test/cli/aop/Main.java
+++ SpringFramework-Core/spring-web/src/main/java/kr/co/test/cli/aop/Main.java
@@ -1,9 +1,28 @@
 package kr.co.test.cli.aop;
 
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.springframework.aop.framework.ProxyFactory;
+
 public class Main {
     public static void main(String[] args) {
-        Pojo pojo = new SimplePojo();
+        ProxyFactory factory = new ProxyFactory(new SimplePojo());
+        factory.addInterface(Pojo.class);
+        factory.addAdvice(new RetryAdvice());
+        System.out.println(">>>");
+        Pojo pojo = (Pojo) factory.getProxy();
         pojo.foo();
+        System.out.println(">>>");
+    }
+}
+
+class RetryAdvice implements MethodInterceptor {
+    @Override
+    public Object invoke(MethodInvocation methodInvocation) throws Throwable {
+        System.out.println("Before");
+        Object proceed = methodInvocation.proceed(); // Plain Object를 실행시키기 위해선 methodInvocation.proceed() 를 호출해야 한다.
+        System.out.println("After");
+        return proceed;
     }
 }
 
